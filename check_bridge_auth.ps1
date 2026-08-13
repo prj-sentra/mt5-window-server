3bf5b87424387c7678391a5c3efa4f79ea995e12
seoho kim <seoho7777.kim@gmail.com>
2026-08-13 13:00:26 +0000

    show only current bridge authentication events

--- check_bridge_auth.ps1
+++ check_bridge_auth.ps1
@@ -17,7 +17,25 @@ $patterns = @(
     'request failed'
 )
 
-Get-Content -LiteralPath $log -Tail 5000 |
+$lines = @(Get-Content -LiteralPath $log -Tail 5000)
+$latestStart = -1
+for ($index = $lines.Count - 1; $index -ge 0; $index--) {
+    if ($lines[$index] -match 'logging initialized') {
+        $latestStart = $index
+        break
+    }
+}
+if ($latestStart -ge 0) {
+    $lines = @($lines[$latestStart..($lines.Count - 1)])
+}
+
+$matched = @($lines |
     Select-String -SimpleMatch -Pattern $patterns |
     Select-Object -Last $Tail |
-    ForEach-Object { $_.Line }
+    ForEach-Object { $_.Line })
+
+if ($matched.Count -eq 0) {
+    'No authentication or connection events in the current bridge process.'
+} else {
+    $matched
+}
